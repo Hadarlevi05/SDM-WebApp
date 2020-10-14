6c477574549ffb82ac58b4466455fba107005040
Hadar <hadarlevi05@gmail.com>
2020-10-14 09:42:51 +0300

    first commit

---
+++ UI/web/Pages/loginPage/LoginPage.js
@@ -0,0 +1,11 @@
+
+
+function showLoginForm() {
+    document.querySelector('#loginForm').style.display = '';
+    document.querySelector('#signupForm').style.display = 'none';
+}
+function showSignupForm(){
+    document.querySelector('#loginForm').style.display = 'none';
+    document.querySelector('#signupForm').style.display = '';
+}
+
